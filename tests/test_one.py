7e77267a3f5bda55e6f920b4a8a8387ae5a63dd2
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2021-12-30 22:14:20 +0100

    tests

---
+++ tests/test_one.py
@@ -0,0 +1,7 @@
+import pytest
+
+def test_func_fast():
+    assert 1==1
+
+def test_fuinc_fast2():
+    assert 2==2
---
+++ tests/test_two.py
@@ -0,0 +1,7 @@
+import pytest
+
+def test_func_2fast():
+    assert 3==3
+
+def test_fuinc_2fast2():
+    assert 4==4
