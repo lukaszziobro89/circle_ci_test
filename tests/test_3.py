40e8646ba990663229c1484f40c6af1d87a2fab7
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2021-12-30 22:55:00 +0100

    fix yml and tests

---
+++ tests/test_3.py
@@ -0,0 +1,14 @@
+import pytest
+
+def test_func_2fast():
+    assert 3==3
+
+def test_fuinc_2fast3():
+    assert 4==4
+
+def test_func_2fast4():
+    assert 3==3
+
+def test_fuinc_2fast21():
+    assert 4==4
+
